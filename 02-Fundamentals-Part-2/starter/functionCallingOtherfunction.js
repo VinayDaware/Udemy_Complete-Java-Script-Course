de165de815f6f4735fe0050d130de6ad1491b08f
unknown <dawarevinay@gmail.com>
2025-06-17 18:58:38 +0530

    function review

--- 02-Fundamentals-Part-2/starter/functionCallingOtherfunction.js
+++ 02-Fundamentals-Part-2/starter/functionCallingOtherfunction.js
@@ -14,3 +14,25 @@ function fruitPrecessor (apples, oranges) {
     return juice;
 }
 fruitPrecessor(6,8);
+
+// calling function and taking decision
+const calcAge= function(birthYear){
+    const currentYear = 2037;
+    return currentYear - birthYear;
+}
+
+const yearsUntilRetirement = function (birthYear, firstName){
+    const age = calcAge(birthYear);
+    const retirement = 65-age;
+    
+    if(retirement>0){
+        console.log (`${firstName} retires in ${retirement} years`)
+        return retirement;
+    }else{
+        console.log(`${firstName} has already retired 🎉`)
+        return -1;    // return statement immediately exit the function that means if we return anything after the return it will not considered
+    }
+}
+
+console.log(yearsUntilRetirement(1991,'Vinay'));
+console.log(yearsUntilRetirement(1960,'Gourav'));
