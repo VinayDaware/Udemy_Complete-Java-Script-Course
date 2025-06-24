8d45dae6663f296430b388451ac3c37436e79108
unknown <dawarevinay@gmail.com>
2025-06-24 18:37:10 +0530

    24 june 25 Object intro

---
+++ 02-Fundamentals-Part-2/starter/objectIntro.js
@@ -0,0 +1,12 @@
+// Object is non premitive data type that is used to stored data in key value pair.
+//The key value pair is known as properties,key value is known as property
+//If the property name is function then it is known as method
+
+const vinay = {
+    firstName : 'Vinay',
+    lastName : 'Daware',
+    age: 2025-1995,
+    city : 'Nagpur',
+    friends : ['Gaurav','Kartik','Nehal','Pranay']
+    
+}
